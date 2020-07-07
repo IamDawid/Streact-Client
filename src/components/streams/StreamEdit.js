1dbc318eef7545dc688e9217523d14ea8e281660
Dawid <david.d.bialy@gmail.com>
2020-07-07 15:58:47 +0100

    StreamEdit now also uses StreamForm and it's initial form values are taken from {this.props.stream}

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);  //stream needs to be fetched right away to make sure this component works in isolation
     }  
+
+    onSubmit = (formValues) => {
+
+
+
+    };
+
     render() {
         console.log(this.props);
         if (!this.props.stream) {
             return <div>Loading stream...</div>;
         }
 
-        return <div>{this.props.stream.title}</div>;
+        return (
+
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} initialValues={this.props.stream} />
+            </div>
+
+            );
     }
 }
 
@@ -20,4 +35,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
