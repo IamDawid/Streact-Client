cc2f572d7b33ce11d27337cc18d84bc47961335f
Dawid <david.d.bialy@gmail.com>
2020-07-08 18:42:46 +0100

    Edit form's initialValues are being selectively picked using lodash _.pick()

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
@@ -24,7 +25,7 @@ class StreamEdit extends React.Component {
 
             <div>
                 <h3>Edit a Stream</h3>
-                <StreamForm onSubmit={this.onSubmit} initialValues={this.props.stream} />
+                <StreamForm onSubmit={this.onSubmit} initialValues={_.pick(this.props.stream, 'title', 'description')} />
             </div>
 
             );
